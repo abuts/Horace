4488172e4f25feb73d7e19cb1a755d5ec5a33262
abuts <Alex.Buts@stfc.ac.uk>
2026-07-07 16:24:31 +0100

    Re #1954 reverting changes to gen_sqw as they are implemented in gen_runfiles

--- horace_core/algorithms/gen_sqw.m
+++ horace_core/algorithms/gen_sqw.m
@@ -362,10 +362,10 @@ if nindx==1
         disp('--------------------------------------------------------------------------------')
     end
 else
-
-    % expand run_ids for replicated files to make run_id-s unique
-    run_files = update_duplicated_rf_id(run_files);
-
+    if opt.replicate && ~spe_unique
+        % expand run_ids for replicated files to make run_id-s unique
+        run_files = update_duplicated_rf_id(run_files);
+    end
     keep_par_cl_running = ~opt.tmp_only || nargout>3;
 
     % Generate unique temporary sqw files, one for each of the spe files
